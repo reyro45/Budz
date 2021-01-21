860bd42f7533d706c423acb25e1b12a5aac4f590
reyro45 <59561156+reyro45@users.noreply.github.com>
2021-01-21 08:01:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,1 @@
-clear
-echo -e "\e[31mWhat system do you have ?\e[0m";
-echo
-echo -e "1. Linux (not finished)";
-echo -e "2. Termux";
-echo
-read -p '>>> ' userinput1;
-if [ "${userinput1:-}" = "1" ]
-then
-        cd linux && bash linux.sh
-fi
-
-if [ "${userinput1:-}" = "2" ]
-then
-        cd tmux && bash run.sh
-fi
+cd tmux && bash run.sh
